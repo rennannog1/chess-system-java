87f13d7403620e8d6bf74bedaa9c7a221dd195c2
Rennan Nogueira <rmelo.nogueira123@gmail.com>
2022-09-02 14:47:18 -0300

    possible moves for a piece

--- src/boardgame/Piece.java
+++ src/boardgame/Piece.java
@@ -1,6 +1,8 @@
 package boardgame;
 
-public class Piece {
+import chess.ChessPiece;
+
+public abstract class Piece {
 	protected Position position;
 	private Board board;
 
@@ -13,15 +15,22 @@ public class Piece {
 		return board;
 	}
 
-	/*static boolean possibleMoves() {
-		return;
+	public abstract boolean[][] possibleMoves();
+
+	public boolean possibleMove(Position position) {
+		return possibleMoves()[position.getRow()][position.getColumn()];
 	}
 
-	boolean possibleMove(Position position) {
-		return;
-	}
+	public boolean isThereAnyPossibleMove() {
+		boolean[][] mat = possibleMoves();
+		for (int i = 0; i < mat.length; i++) {
+			for (int j = 0; j < mat.length; j++) {
+				if (mat[i][j]) {
+					return true;
+				}
+			}
 
-	boolean isThereAnyPossibleMove() {
-		return;
-	}*/
+		}
+		return false;
+	}
 }
