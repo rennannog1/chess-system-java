5ad646a4d912c26de6d1e331fd90eec48c0b037b
Rennan Nogueira <rmelo.nogueira123@gmail.com>
2022-09-02 11:27:15 -0300

    Acerto funcao fromPosition

--- src/chess/ChessPosition.java
+++ src/chess/ChessPosition.java
@@ -25,7 +25,7 @@ public class ChessPosition {
 		return new Position (8 - row, column - 'a');
 	}
 	
-	protected ChessPosition toPosition(Position position) {
+	protected static ChessPosition fromPosition(Position position) {
 		return new ChessPosition ((char) ('a' - position.getColumn()), 8 - position.getRow());
 	}
 
